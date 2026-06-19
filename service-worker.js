c5e60eb1dabaf62841aaa8a9b0be9de4cadd3ece
gameonxr <kprincerx09@gmail.com>
2026-06-20 00:08:08 +0530

    Refactor service worker for cache management

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,29 @@
-const CACHE_NAME = "novasocial-v1";
+const CACHE_NAME = "my-pwa-v1";
 
-const ASSETS = [
+const FILES = [
   "./",
   "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./manifest.json"
 ];
 
-// INSTALL
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      return cache.addAll(FILES);
     })
   );
   self.skipWaiting();
 });
 
-// ACTIVATE
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches.keys().then((keys) =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
   );
-
-  // IMPORTANT: instantly take control
-  event.waitUntil(self.clients.claim());
+  return self.clients.claim();
 });
 
-// FETCH (offline support)
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
